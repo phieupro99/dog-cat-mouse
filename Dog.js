e67b85c8b807e0a9986095cd8b6ab45d53ee7b4e
Phuong Hieu <phieu221199@gmail.com>
2019-05-04 23:25:24 +0700

    add say Hi fixed

--- Dog.js
+++ Dog.js
@@ -1,13 +1,15 @@
+var chalk = require('chalk');
+
 function Dog(name){
 	this.stomach = [];
 	this.name = name;
 }
 
-Dog.prototype.eat = fucntion(cat){
+Dog.prototype.eat = function(cat){
 	this.stomach.push(cat);
-};
+}
 Dog.prototype.sayHi = function(){
 	console.log('Hi! I am  a dog. My name is '+ chalk.blue(this.name));
-};
+}
 
 module.exports = Dog;
